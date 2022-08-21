1adbb2dcf909d5ae4e70718f47036b5f4f6d1ad4
priyanka <111413747+PriyankaA121@users.noreply.github.com>
2022-08-21 09:07:51 -0700

    Add files via upload

---
+++ priyanka sharma sql2 mini project.sql
@@ -0,0 +1,58 @@
+create database mini_project_sql2;
+use mini_project_sql2;
+select * from icc_testbatting_figures as test;
+#1.	Import the csv file to a table in the database.
+
+
+
+#2.	Remove the column 'Player Profile' from the table.
+
+
+#3.	Extract the country name and player names from the given data and store it in seperate columns for further usage.
+
+
+
+
+#4.	From the column 'Span' extract the start_year and end_year and store them in seperate columns for further usage.
+
+
+
+
+
+
+#5.	The column 'HS' has the highest score scored by the player so far in any given match. 
+#The column also has details if the player had completed the match in a NOT OUT status. 
+#Extract the data and store the highest runs and the NOT OUT status in different columns.
+
+
+
+#6.	Using the data given, considering the players who were active in the year of 2019, 
+#create a set of batting order of best 6 players using the selection criteria of those
+# who have a good average score across all matches for India.
+
+
+
+
+
+#7.	Using the data given, considering the players who were active in the year of 2019, 
+#create a set of batting order of best 6 players using the selection criteria of those 
+#who have highest number of 100s across all matches for India.
+
+
+#8.	Using the data given, considering the players who were active in the year of 2019,
+# create a set of batting order of best 6 players using 2 selection criterias of your own for India.
+
+
+
+#9.	Create a View named ‘Batting_Order_GoodAvgScorers_SA’ using the data given, 
+#considering the players who were active in the year of 2019, create a set of
+# batting order of best 6 players using the selection criteria of those 
+#who have a good average score across all matches for South Africa.
+
+
+
+
+#10.	Create a View named ‘Batting_Order_HighestCenturyScorers_SA’ Using the data given,
+# considering the players who were active in the year of 2019, create a set of batting order of 
+#best 6 players using the selection criteria of those who have highest number of 100s across all matches for South Africa.
+
